bed91112be1773b016b3d9bc2ae5586deb746fed
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-05-16 16:09:38 -0400

    Rotated definitions in GravityDirection

--- source/entities/GravityDirection.hx
+++ source/entities/GravityDirection.hx
@@ -4,10 +4,10 @@ import flixel.math.FlxPoint;
 import flixel.FlxObject;
 
 class GravityDirection {
-  public static var Up = new GravityDirection(FlxObject.UP, new FlxPoint(0, -G), 90, FlxObject.UP | FlxObject.DOWN, Orientation.Vertical);
-  public static var Down = new GravityDirection(FlxObject.DOWN, new FlxPoint(0, G), 270, FlxObject.UP | FlxObject.DOWN, Orientation.Vertical);
-  public static var Left = new GravityDirection(FlxObject.LEFT, new FlxPoint(-G, 0), 180, FlxObject.LEFT | FlxObject.RIGHT, Orientation.Horizontal);
-  public static var Right = new GravityDirection(FlxObject.RIGHT, new FlxPoint(G, 0), 0, FlxObject.LEFT | FlxObject.RIGHT, Orientation.Horizontal);
+  public static var Up = new GravityDirection(FlxObject.UP, new FlxPoint(0, -G), 180, FlxObject.UP | FlxObject.DOWN, Orientation.Vertical);
+  public static var Down = new GravityDirection(FlxObject.DOWN, new FlxPoint(0, G), 0, FlxObject.UP | FlxObject.DOWN, Orientation.Vertical);
+  public static var Left = new GravityDirection(FlxObject.LEFT, new FlxPoint(-G, 0), 90, FlxObject.LEFT | FlxObject.RIGHT, Orientation.Horizontal);
+  public static var Right = new GravityDirection(FlxObject.RIGHT, new FlxPoint(G, 0), 270, FlxObject.LEFT | FlxObject.RIGHT, Orientation.Horizontal);
 
   public static inline var G : Float = 48;
 
