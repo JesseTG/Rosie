ec673839100e4f22916812f22d310c2f3ce66706
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-05-16 09:53:18 -0400

    Add a gravity member to Block

--- source/entities/Block.hx
+++ source/entities/Block.hx
@@ -7,6 +7,8 @@ import flixel.graphics.frames.FlxFramesCollection;
 class Block extends FlxSprite {
   public var blockColor(default, null) : BlockColor;
 
+  public var gravity(default, set) : GravityDirection;
+
   public function new(x:Float = 0, y:Float = 0, sprites:FlxFramesCollection, blockColor:BlockColor) {
     super(x, y);
     this.frame = sprites.getByName(cast(blockColor));
@@ -14,7 +16,20 @@ class Block extends FlxSprite {
     this.flixelType = FlxType.OBJECT;
     this.updateHitbox();
     this.resetSizeFromFrame();
+    // TODO: Adjust the hitbox whenever gravity changes
+  }
 
+  public function set_gravity(gravity:GravityDirection) {
+    this.gravity = gravity;
+    this.velocity.x = this.gravity.Gravity.x;
+    this.velocity.y = this.gravity.Gravity.y;
+    // I could assign velocity directly, but I don't know if that would be
+    // copy assignment or reference assignment; better play it safe
+
+    this.allowCollisions = this.gravity.CollisionSides;
+
+    return this.gravity;
+  }
 
   }
 }
