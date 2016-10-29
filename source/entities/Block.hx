88945ba2ab5d183ab77f2c21d80e3c881eddc796
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-10-28 22:18:05 -0400

    Remove crap from Block

--- source/entities/Block.hx
+++ source/entities/Block.hx
@@ -9,34 +9,13 @@ import flixel.FlxG;
 
 class Block extends FlxSprite {
   public var blockColor(default, null) : BlockColor;
-  // TODO: Make a setter that also sets the image
 
   public function new(x:Float = 0, y:Float = 0, sprites:FlxFramesCollection, blockColor:BlockColor) {
     super(x, y);
     this.frame = sprites.getByName(cast(blockColor));
     this.blockColor = blockColor;
-    this.flixelType = FlxType.OBJECT;
     this.updateHitbox();
     this.resetSizeFromFrame();
-  }
-
-  public function snapToGrid() {
-    this.x = Math.round(this.x / this.frameWidth) * this.frameWidth;
-    this.y = Math.round(this.y / this.frameHeight) * this.frameHeight;
-  }
-
-  public override function update(elapsed:Float) {
-
-
-    super.update(elapsed);
-
-
-    if (FlxG.debugger.visible && !this.moves) {
-      this.color = 0xFF0000;
-    }
-    else {
-      this.color = 0xFFFFFF;
-    }
   }
 
   public override function toString() : String {
