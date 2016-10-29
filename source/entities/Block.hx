591e51101c30f911098fd5fcc3f73ec99d9e1d69
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-10-29 01:44:15 -0400

    Add a Block.blockColor setter

--- source/entities/Block.hx
+++ source/entities/Block.hx
@@ -4,14 +4,23 @@ import flixel.FlxSprite;
 import flixel.graphics.frames.FlxFramesCollection;
 
 class Block extends FlxSprite {
-  public var blockColor(default, null) : BlockColor;
+  private var sprites:FlxFramesCollection;
+  public var blockColor(default, set) : BlockColor;
 
   public function new(x:Float = 0, y:Float = 0, sprites:FlxFramesCollection, blockColor:BlockColor) {
     super(x, y);
+    this.sprites = sprites;
     this.frame = sprites.getByName(cast(blockColor));
     this.blockColor = blockColor;
     this.updateHitbox();
     this.resetSizeFromFrame();
+  }
+
+  public function set_blockColor(blockColor:BlockColor) {
+    this.blockColor = blockColor;
+    this.frame = sprites.getByName(cast(blockColor));
+
+    return blockColor;
   }
 
   public override function toString() : String {
