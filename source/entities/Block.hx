6b5753fe936b36ab02120c5f7ce89fd07dd607e5
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-10-28 21:27:35 -0400

    Remove more stuff from Block

--- source/entities/Block.hx
+++ source/entities/Block.hx
@@ -1,7 +1,6 @@
 package entities;
 
 import flixel.math.FlxMath;
-import flixel.math.FlxPoint;
 import flixel.FlxObject;
 import flixel.FlxBasic.FlxType;
 import flixel.FlxSprite;
@@ -12,35 +11,13 @@ class Block extends FlxSprite {
   public var blockColor(default, null) : BlockColor;
   // TODO: Make a setter that also sets the image
 
-  public var gravity(default, set) : GravityDirection;
-
-  public var targetCell: FlxPoint;
-
   public function new(x:Float = 0, y:Float = 0, sprites:FlxFramesCollection, blockColor:BlockColor) {
     super(x, y);
     this.frame = sprites.getByName(cast(blockColor));
-    this.moves = false;
     this.blockColor = blockColor;
     this.flixelType = FlxType.OBJECT;
-    this.targetCell = new FlxPoint();
-    //this.allowCollisions = FlxObject.UP | FlxObject.DOWN;
-    this.pixelPerfectPosition = true;
-    //this.pixelPerfectRender = true;
     this.updateHitbox();
     this.resetSizeFromFrame();
-    // TODO: Adjust the hitbox whenever gravity changes
-  }
-
-  public function set_gravity(gravity:GravityDirection) {
-    this.gravity = gravity;
-    this.velocity.x = this.gravity.Gravity.x;
-    this.velocity.y = this.gravity.Gravity.y;
-    // I could assign velocity directly, but I don't know if that would be
-    // copy assignment or reference assignment; better play it safe
-
-    this.allowCollisions = this.gravity.CollisionSides;
-
-    return this.gravity;
   }
 
   public function snapToGrid() {
