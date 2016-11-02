b79f180d17c916d7f8c13830f19560dff077f539
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-11-02 18:47:26 -0400

    Separate the computing and the granting of the time bonus

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -223,8 +223,11 @@ class PlayState extends FlxState
     });
   }
 
-  private function _addBonusTime(blocksCreated:Int) {
-    var bonus = blocksCreated * 0.05;
+  private function _grantBonusTime(blocksCreated:Int) {
+    this._addBonusTime(blocksCreated * 0.05);
+  }
+
+  private function _addBonusTime(bonus:Float) {
     _time += bonus;
     _timeChangeDisplay.color = FlxColor.GREEN;
     _timeChangeDisplay.text = Printf.format("+%.1f", [bonus]);
