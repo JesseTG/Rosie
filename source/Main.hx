20c3490696783dd3a687687d547f69684b88fb2f
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-11-10 17:06:05 -0500

    In debug mode, trace when certain FlxG signals are dispatched

--- source/Main.hx
+++ source/Main.hx
@@ -10,6 +10,12 @@ class Main extends Sprite
   {
     super();
 
+#if debug
+    FlxG.signals.focusGained.add(function() trace("focusGained"));
+    FlxG.signals.focusLost.add(function() trace("focusLost"));
+    FlxG.signals.stateSwitched.add(function() trace("stateSwitched"));
+#end
+
     FlxG.fixedTimestep = false;
     addChild(new FlxGame(320, 240, MenuState, 2.0));
     // TODO: Make a base State class that holds the background and make
