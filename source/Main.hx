1945b02d56813ba90421c01ca0eafa233cf766b7
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-11-12 19:18:58 -0500

    Move states to the states folder

--- source/Main.hx
+++ source/Main.hx
@@ -11,6 +11,8 @@ import cpp.vm.Gc;
 import neko.vm.Gc;
 #end
 
+import states.MenuState;
+
 class Main extends Sprite
 {
   public function new()
--- source/states/MenuState.hx
+++ source/states/MenuState.hx
@@ -1,4 +1,4 @@
-package;
+package states;
 
 import flixel.addons.display.FlxBackdrop;
 import flixel.addons.editors.tiled.TiledImageLayer;
