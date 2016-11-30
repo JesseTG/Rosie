9d08c033f4799b6431fb0baa180d9e538b0207f7
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-11-30 16:10:19 -0500

    Initial commit of Rosie.hx

---
+++ source/entities/Rosie.hx
@@ -0,0 +1,14 @@
+package entities;
+
+import flixel.animation.FlxAnimation;
+import flixel.animation.FlxAnimationController;
+import flixel.FlxSprite;
+import flixel.graphics.frames.FlxFramesCollection;
+
+class Rosie extends FlxSprite {
+  public function new(x:Int, y:Int, sprites:FlxFramesCollection) {
+    super(x, y);
+
+
+  }
+}
