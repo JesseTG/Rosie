b2dbf3499e230226c3b635b2450920915a8184a6
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-12-11 19:26:30 -0500

    Implement more of Rosie's animation

--- source/entities/Rosie.hx
+++ source/entities/Rosie.hx
@@ -6,10 +6,16 @@ import flixel.animation.FlxAnimationController;
 import flixel.FlxSprite;
 import flixel.FlxObject;
 import flixel.graphics.frames.FlxFramesCollection;
+import flixel.addons.util.FlxFSM;
+import flixel.addons.util.FlxFSM.FlxFSMState;
+import flixel.addons.util.FlxFSM.FlxFSMTransitionTable;
 
 class Rosie extends FlxSprite {
   private static inline var IDLE_FPS = 6;
-  private static inline var WALK_FPS = 6;
+  private static inline var RUN_FPS = 12;
+
+  public var fsm : FlxFSM<Rosie>;
+
   public function new(x:Int, y:Int, sprites:FlxFramesCollection) {
     super(x, y);
 
@@ -18,6 +24,7 @@ class Rosie extends FlxSprite {
     this.updateHitbox();
     this.setFacingFlip(FlxObject.LEFT, true, false);
     this.setFacingFlip(FlxObject.RIGHT, false, false);
+    this.facing = FlxObject.RIGHT;
 
     var idleFrames = [for (i in 1...62) Printf.format("cat/idle/rosie-idle-%02d.png", [i])];
     var runFrames = [for (i in 1...7) Printf.format("cat/run/rosie-run-%02d.png", [i])];
@@ -29,18 +36,70 @@ class Rosie extends FlxSprite {
       true
     );
     this.animation.addByNames(
-      "walk",
-      runFrames,
-      WALK_FPS,
-      true
-    );
-    this.animation.addByNames(
       "run",
       runFrames,
-      WALK_FPS * 2,
+      RUN_FPS,
       true
     );
 
-    this.animation.play("idle");
+    this.frame = sprites.getByName(idleFrames[0]);
+    this.fsm = new FlxFSM<Rosie>(this);
+    this.fsm.transitions
+      .add(RosieIdleState, RosieRunState, function(rosie) {
+        return Math.random() < 0.004;
+      })
+      .add(RosieRunState, RosieIdleState, function(rosie) {
+        return (rosie.fsm.age >= 1.0) ? Math.random() < 0.4 : false;
+      })
+      .add(RosieIdleState, RosieJumpState, function(rosie) {
+        return false;
+      })
+      .add(RosieJumpState, RosieIdleState, function(rosie) {
+        return false;
+      })
+      .add(RosieRunState, RosieJumpState, function(rosie) {
+        return false;
+      })
+      .add(RosieJumpState, RosieRunState, function(rosie) {
+        return false;
+      })
+      .start(RosieIdleState);
+  }
+
+  public override function update(elapsed:Float) {
+    this.fsm.update(elapsed);
+    super.update(elapsed);
+  }
+
+  public override function destroy() {
+    this.fsm.destroy();
+    this.fsm = null;
+    super.destroy();
+  }
+}
+
+private class RosieIdleState extends FlxFSMState<Rosie> {
+  public override function enter(owner:Rosie, fsm:FlxFSM<Rosie>) {
+    owner.animation.play("idle");
+  }
+
+  public override function update(elapsed:Float, owner:Rosie, fsm:FlxFSM<Rosie>) {
+  }
+}
+
+private class RosieRunState extends FlxFSMState<Rosie> {
+  public override function enter(owner:Rosie, fsm:FlxFSM<Rosie>) {
+    owner.animation.play("run");
+  }
+
+  public override function update(elapsed:Float, owner:Rosie, fsm:FlxFSM<Rosie>) {
+  }
+}
+
+private class RosieJumpState extends FlxFSMState<Rosie> {
+  public override function enter(owner:Rosie, fsm:FlxFSM<Rosie>) {
+  }
+
+  public override function update(elapsed:Float, owner:Rosie, fsm:FlxFSM<Rosie>) {
   }
 }
