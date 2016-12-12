de1360fa6b0acde84a5daa65fddbed7b3ec9382a
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-12-12 16:43:28 -0500

    Add some emotes and animations for Rosie

--- source/entities/RosieEmote.hx
+++ source/entities/RosieEmote.hx
@@ -14,21 +14,88 @@ import flixel.addons.display.FlxNestedSprite;
 import flixel.math.FlxMath;
 
 class RosieEmote extends FlxNestedSprite {
+  public var state(default, set) : EmoteState;
 
   public function new(x:Float, y:Float, sprites:FlxFramesCollection) {
     super(x, y);
 
     this.frames = sprites;
-    this.frame = sprites.getByName("emotes/happyEmote.png");
-    pixelPerfectRender = true;
-    pixelPerfectPosition = true;
+    this.frame = sprites.getByName("emotes/happy-00.png");
+    this.pixelPerfectRender = true;
+    this.pixelPerfectPosition = true;
     this.resetSizeFromFrame();
     this.updateHitbox();
+
+    this.animation.addByNames(cast Happy, [for (i in 0...4) "emotes/happy-00.png"], 1, false);
+    this.animation.addByStringIndices(
+      cast Bored,
+      "emotes/bored-",
+      ["00", "01", "00", "01", "00", "01", "00", "01", "00", "00", "00"],
+      ".png",
+      1,
+      false
+    );
+
+    this.animation.addByStringIndices(
+      cast Confused,
+      "emotes/confused-",
+      ["00", "01", "00", "01", "00", "01", "00", "01", "00", "00", "00"],
+      ".png",
+      1,
+      false
+    );
+
+    this.animation.addByStringIndices(
+      cast Doh,
+      "emotes/doh-",
+      ["00", "01", "00", "01", "00", "01", "00", "01", "00", "00", "00"],
+      ".png",
+      1,
+      false
+    );
+
+    this.animation.addByStringIndices(
+      cast Angry,
+      "emotes/angry-",
+      ["00", "01", "00", "01", "00", "01", "00", "01", "00", "00", "00"],
+      ".png",
+      1,
+      false
+    );
+
+    this.animation.addByStringIndices(
+      cast VeryHappy,
+      "emotes/very-happy-",
+      ["00", "01", "00", "01", "00", "01", "00", "01", "00", "00", "00"],
+      ".png",
+      2,
+      false
+    );
+    this.animation.finishCallback = function(_) {
+      this.state = None;
+    };
+    this.state = Angry;
+  }
+
+  private inline function set_state(s:EmoteState) {
+    this.state = s;
+    this.visible = (this.state != None);
+    if (s != None) {
+      this.animation.play(cast this.state);
+    }
+    return this.state;
   }
 
 }
 
-enum EmoteState {
-  Happy;
-  Bored;
+@:enum
+@:notNull
+abstract EmoteState(String) {
+  var None = "none";
+  var Happy = "happy";
+  var Bored = "bored";
+  var VeryHappy = "very-happy";
+  var Confused = "confused";
+  var Doh = "doh";
+  var Angry = "angry";
 }
