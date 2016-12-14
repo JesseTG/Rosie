513587db90b7b2734ea1ee458f7cceb4d16bb902
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-12-14 13:57:18 -0500

    Make the helper functions inline

--- source/util/ArrayTools.hx
+++ source/util/ArrayTools.hx
@@ -3,7 +3,7 @@ package util;
 import de.polygonal.core.util.Assert.assert;
 
 class ArrayTools {
-  public static function swap<T>(array:Array<T>, a:Int, b:Int) {
+  public static inline function swap<T>(array:Array<T>, a:Int, b:Int) {
     assert(array != null);
     assert(0 <= a && a < array.length);
     assert(0 <= b && b < array.length);
@@ -15,7 +15,7 @@ class ArrayTools {
     }
   }
 
-  public static function getFront<T>(array:Array<T>, index:Int) : T {
+  public static inline function getFront<T>(array:Array<T>, index:Int) : T {
     assert(array != null);
     assert(0 <= index && index < array.length);
 
