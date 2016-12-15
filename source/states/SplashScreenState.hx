fa3b6034eb3cfee4e9791cf3ec13609e07c11726
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-12-15 17:22:31 -0500

    Add SplashScreenState.hx

---
+++ source/states/SplashScreenState.hx
@@ -0,0 +1,38 @@
+package states;
+
+import de.polygonal.Printf;
+import flixel.addons.editors.tiled.TiledImageLayer;
+import flixel.addons.editors.tiled.TiledMap;
+import flixel.addons.editors.tiled.TiledObject;
+import flixel.addons.editors.tiled.TiledObjectLayer;
+import flixel.addons.editors.tiled.TiledPropertySet;
+import flixel.addons.editors.tiled.TiledTileLayer;
+import flixel.addons.editors.tiled.TiledTileSet;
+import flixel.text.FlxText.FlxTextAlign;
+import flixel.text.FlxText.FlxTextBorderStyle;
+import flixel.tile.FlxTilemap;
+import flixel.group.FlxSpriteGroup;
+import flixel.ui.FlxBitmapTextButton;
+import flixel.text.FlxBitmapText;
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.math.FlxPoint;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.util.FlxColor;
+import flixel.graphics.frames.FlxBitmapFont;
+import flixel.addons.effects.chainable.FlxEffectSprite;
+import flixel.addons.display.FlxStarField.FlxStarField2D;
+import flixel.addons.display.FlxStarField.FlxStarField3D;
+import flixel.addons.effects.chainable.FlxOutlineEffect;
+import flixel.addons.effects.chainable.FlxOutlineEffect.FlxOutlineMode;
+import flixel.tweens.FlxEase;
+import flixel.input.mouse.FlxMouseEventManager;
+import flixel.tweens.FlxTween;
+
+using ObjectInit;
+using Lambda;
+
+class SplashScreenState extends FlxState {
+
+}
