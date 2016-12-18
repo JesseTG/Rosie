5a01aa0973cf0e7055f1bc9979fa99b9d73807a8
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-12-18 17:44:03 -0500

    Some extra info to Main

--- source/Main.hx
+++ source/Main.hx
@@ -31,12 +31,20 @@ class Main extends Sprite
     FlxG.signals.stateSwitched.add(function() Gc.run(true));
 #end
 
-    FlxG.fixedTimestep = false;
     FlxG.signals.gameStarted.add(function() {
+      FlxG.fixedTimestep = true;
       FlxG.camera.pixelPerfectRender = true;
+      FlxG.camera.filtersEnabled = false;
+      FlxG.game.filtersEnabled = false;
+      trace('Render Mode: ${FlxG.renderMethod}');
+      trace('Mobile Device: ${FlxG.onMobile}');
+
+#if FLX_RENDER_TRIANGLE
+      trace("FLX_RENDER_TRIANGLE enabled");
+#end
     });
 
-    addChild(new FlxGame(320, 240, SplashScreenState, 2.0));
+    addChild(new FlxGame(320, 240, SplashScreenState, 2.0, 60, 60, true));
 
     // TODO: Make exceptions bring the player to an error screen gracefully
     // TODO: Make a proper package hierarchy
